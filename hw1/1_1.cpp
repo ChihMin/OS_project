eb8cc84870ad2f760dc166ef9cee4029a2fac316
ChihMin <s102062111@m102.nthu.edu.tw>
2014-10-07 13:46:34 +0800

    1-1 signal printedgit add -A Work fine git add -A

--- hw1/1_1.cpp
+++ hw1/1_1.cpp
@@ -1,19 +1,42 @@
 #include <cstdio>
 #include <cstdlib>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <cstring>
+#include <errno.h>
 using namespace std;
 
-int main(){
-	int s[100] ; 
-	int n ; 
-	scanf("%d", &n) ; 
 
-	for(int i=0;i<n;++i)
-		scanf("%d",&s[i]);
-
-	for(int i=n-1;i>=0;--i)
-		printf("%d\n",s[i]); 
+int main( int argc , char *argv[] ){
+	int status ; 
+	pid_t pid = fork() ;	
+	pid_t mypid = getpid() ; 
 	
+	for(int i=0;i<10;++i)	printf("=");
+	printf("\n");
+	printf("I'm parent!! My pid is %d\n",mypid);
+	printf("And my child's pid = %d\n",pid);
+
+	if( pid < 0 ){
+		printf("fork error\n");
+		exit( -1 ) ;
+	}
+	else if( pid == 0 ){
+		printf("=====\n");
+		execl( argv[1] ,NULL );
+		exit( 1 ) ;
+	printf("=====\n");
+	}
+	else{
+		wait( &status ); 
+		//printf("Child Complete\n");
+		printf("Signal: %d\nStatus: %d\nEXITED: %d\n", WIFSIGNALED(status), status, WIFEXITED( status ) ) ;
+		 
+		exit( 0 ) ;
+	}
+	for(int i=0;i<10;++i)	printf("=");
 
 	return 0 ; 
 }
