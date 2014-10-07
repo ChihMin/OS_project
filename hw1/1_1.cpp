bb8b33ea864e03939740a24e201e66fdb2228319
ChihMin <s102062111@m102.nthu.edu.tw>
2014-10-07 14:13:34 +0800

    Segmentation fault process yetgit add -A

--- hw1/1_1.cpp
+++ hw1/1_1.cpp
@@ -11,32 +11,35 @@ using namespace std;
 
 int main( int argc , char *argv[] ){
 	int status ; 
-	pid_t pid = fork() ;	
-	pid_t mypid = getpid() ; 
+
+	printf("Process fork!\n");
+	printf("Child Process executes Test program!!\n");
 	
-	for(int i=0;i<10;++i)	printf("=");
-	printf("\n");
-	printf("I'm parent!! My pid is %d\n",mypid);
-	printf("And my child's pid = %d\n",pid);
+	pid_t mypid = getpid() ; 
+	printf("Hello! I'm parent process! My pid is %d\n",mypid);
+	pid_t pid = fork() ;	
+	
 
 	if( pid < 0 ){
 		printf("fork error\n");
 		exit( -1 ) ;
 	}
 	else if( pid == 0 ){
-		printf("=====\n");
+		printf("---------CHILD PROCESS BEGIN---------\n");
 		execl( argv[1] ,NULL );
 		exit( 1 ) ;
-	printf("=====\n");
 	}
 	else{
-		wait( &status ); 
-		//printf("Child Complete\n");
-		printf("Signal: %d\nStatus: %d\nEXITED: %d\n", WIFSIGNALED(status), status, WIFEXITED( status ) ) ;
-		 
+		pid_t x ; 
+		x = wait( &status ); 
+		//printf("Signal: %d\nStatus: %d\nEXITED: %d\nx: %d\n", WIFSIGNALED( x ), status, WIFEXITED( x ) ,x) ;
+		
+		printf("---------CHILD PROCESS END---------\n");
+		printf("Receiving the SIGCHLD signal\n\n");
+		printf("Get the status is %d\n",status ) ;
 		exit( 0 ) ;
 	}
-	for(int i=0;i<10;++i)	printf("=");
+	//for(int i=0;i<10;++i)	printf("=");
 
 	return 0 ; 
 }
