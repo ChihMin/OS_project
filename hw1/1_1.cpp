5603d59ecdcfae2f1015daad23819c54715a4b8b
ChihMin <s102062111@m102.nthu.edu.tw>
2014-10-09 00:34:45 +0800

    signal repair

--- hw1/1_1.cpp
+++ hw1/1_1.cpp
@@ -9,41 +9,41 @@
 using namespace std;
 
 
+bool check = true; 
+
+void mySignal( int sig ){
+	int status; 
+	pid_t childPid = wait( &status ) ; 
+	
+
+	if( WIFEXITED( status ) ){
+		printf("The child is terminated success!!\n"); 
+	}
+	else{
+		if( WIFSIGNALED( status ) ){
+			int termsig = WTERMSIG( status ) ;
+			printf("termsig = %d %d\n",status, termsig ) ;  
+		}
+	}
+	check = false ; 
+}
+
+
 int main( int argc , char *argv[] ){
-	int status ; 
-	printf("sig = %d\n",SIGALRM);
-	printf("Process fork!\n");
-	printf("Child Process executes Test program!!\n");
-	
-	pid_t mypid = getpid() ; 
-	printf("Hello! I'm parent process! My pid is %d\n",mypid);
-	pid_t pid = fork() ;	
-	
+		
+	signal( SIGCHLD, mySignal ) ; 
+	pid_t pid = fork() ; 
+
 
 	if( pid < 0 ){
 		printf("fork error\n");
 		exit( -1 ) ;
 	}
 	else if( pid == 0 ){
-		//printf("---------CHILD PROCESS BEGIN---------\n");
-		execl( argv[1], NULL  );
+		execl( argv[1], NULL );
 		exit( 0 ) ;
 	}
-	else{
-		pid_t x ; 
-		x = wait( &status ); 
-		//printf("Signal: %d\nStatus: %d\nEXITED: %d\nx: %d\n", WIFSIGNALED( x ), status, WIFEXITED( x ) ,x) ;
-		int ifSig = WIFSIGNALED( status ) ; 
-		int term = WTERMSIG( status );
 
-		printf("%d %d\n",ifSig, term) ; 
-
-		//printf("---------CHILD PROCESS END---------\n");
-		printf("Receiving the SIGCHLD signal\n\n");
-		printf("Get the status is %d\n",status ) ;
-		exit( 0 ) ;
-	}
-	//for(int i=0;i<10;++i)	printf("=");
-
+	while( check ) ; 
 	return 0 ; 
 }
