48e08c519eb9652720a603263dce01c9c29ec4c4
ChihMin <s102062111@m102.nthu.edu.tw>
2014-10-22 09:19:27 +0800

    fixed some unlock bugs and output format

--- pthread/fpconvar.c
+++ pthread/fpconvar.c
@@ -2,95 +2,91 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 #define NUM_THREAD 3
 
-int isPrime ;	  
-int isFinished = 0 ;
-int lastPrime = 2 ;
-int lastThread = -1;
+int isPrime ; // If a number is prime, let isPrime = 1	  
+int isFinished = 0 ;	// If count is finished, isFinished = 1 ; 
+int lastPrime = 2 ;	// lastprime is used to record last prime < p appearing. 
 int p = 2 ; 
-pthread_mutex_t count_mutex ; 
-pthread_cond_t count_threshold_cv ; 
-int *visit ; 
-int TCOUNT, COUNT_LIMIT ; 
-int Signal = 0 ;
+pthread_mutex_t count_mutex ;	// thread mutex  
+pthread_cond_t count_threshold_cv ; // thread condition variable 
+int TCOUNT, COUNT_LIMIT ; // TCOUNT is the limit of p   
+int Signal = 0 ;	// Signal is used to record whether signal is sent or not
 
 void *inc_count( void *t ){
 	int i , j ; 
-	int my_id = (int)t ; 
-	int result ; 
-	//int visit[100] ; 
+	int my_id = (int)t ; // Thread ID 2 3 
+	int result ; // Is used to delay. 
 	
-	//memset( visit, 0 , sizeof( visit ) ) ; 
 
-	while( p < TCOUNT ){
+	while( p < TCOUNT ){ 
 		pthread_mutex_lock( &count_mutex ) ;  
-		if( isFinished ){	
-			pthread_mutex_lock( &count_mutex ) ; 
-			pthread_exit( NULL ) ; 
+		if( isFinished ){							// if while loop is finished
+			pthread_mutex_unlock( &count_mutex ) ;	// unlock p 
+			pthread_exit( NULL ) ;					// exit thread 
 		}	  
 		
-		if( isPrime	)	lastPrime = p ; 
-		p++;
+		if( isPrime	)	lastPrime = p ;		// if lastnumber is prime, lastprime = p  
+		p++;							// let p add 1 first 
 		
-		// If p is prime, then isPrime = 1
-		isPrime = 1 ;
-		for(int i=2;i < p ;++i ){
+		isPrime = 1 ;					// If p is prime, then isPrime = 1
+		for(int i=2;i < p ;++i ){		// Judge whether p is prime 
 			if( !( p % i )){
 				isPrime = 0 ; 
 				break; 
 			}
 		}
 
-		printf("prime_count() : thread %d, p = %d\n",my_id, p ) ; 
-		if( isPrime )	printf("prime_count() : thread %d, find prime %d\n",my_id, p);
+		printf("prime_count(): thread %d, p = %d\n",my_id, p ) ; 
+		if( isPrime )	printf("prime_count(): thread %d, find prime = %d\n",my_id, p);
 		
 		if( !Signal && isPrime && p >= COUNT_LIMIT ){
-			printf("prime_count() : thread %d, prime = %d prime reached!\n", my_id, p) ;
+			// if signal is not sent and p is prime and p >= COUNT_LIMIT 
+			printf("prime_count(): thread %d, prime = %d prime reached.\n", my_id, p) ;
 
-			pthread_cond_signal( &count_threshold_cv ) ; 
-			printf("Just send signal!!!\n") ;  
-			Signal = 1 ;
-		}
+			pthread_cond_signal( &count_threshold_cv ) ; // sent signal to watch() ;  
+			usleep( 1000 ) ; 
+			printf("Just send signal.\n") ;  
+			Signal = 1 ;	// Signal has been sent 
+		}	
 
-		if( p >= TCOUNT ){
-			isFinished = 1 ;
-			pthread_mutex_unlock( &count_mutex ) ; 
-			for( j = 0; j < 100000; ++j ) result += (double)rand() ;   
-			continue ; 
+		if( p >= TCOUNT ){	
+			isFinished = 1 ;	// is p is finished 
+			pthread_mutex_unlock( &count_mutex ) ; // unlock  p 
+			usleep( 200 ) ;						//delay
+			continue ;							// continue 
 		} 
-		pthread_mutex_unlock( &count_mutex ) ; 	
-		for( j = 0; j < 100000; ++j ) result += (double)rand() ;   
-
+		pthread_mutex_unlock( &count_mutex ) ; // unlock 	
+		usleep( 200 ) ; 
 	}
+	usleep( 1000 ) ; 
 	pthread_exit( NULL ) ; 
 }
 
 void *watch_count( void *t ){
 	int i , j ; 
-	int my_id = (int)t ;
+	int my_id = (int)t ;	// my_id = 1
 	
-	printf("Starting Watch_count() : thread %d, p = %d Going to wait...\n",my_id,p ) ; 
-	pthread_mutex_lock( &count_mutex ) ; 
-	if( p < COUNT_LIMIT ){
-		printf("watch_count() : thread %d going into wait... \n", my_id) ; 
+	printf("Starting Watch_count(): thread %d, p = %d Going to wait...\n",my_id,p ) ; 
+	pthread_mutex_lock( &count_mutex ) ;	// lock
+	if( p < COUNT_LIMIT ){	
+		pthread_cond_wait( &count_threshold_cv, &count_mutex ) ; // wait signal 
 		
-		pthread_cond_wait( &count_threshold_cv, &count_mutex ) ; 
-		
-		printf("watch_count() : thread %d Conditional signal received. p = %d\n",my_id ,p);
-		printf("watch_count() : thread %d Updating the value of p....\n", my_id ) ;
+		printf("watch_count(): thread %d Conditional signal received. p = %d\n",my_id ,p);
+		printf("watch_count(): thread %d Updating the value of p...\n", my_id ) ;
 		printf("the lastest prime found before p = %d\n", lastPrime ) ;  
-		printf("watch_count() : thread %d count p now = %d\n", my_id, p = p + lastPrime ) ;
+		printf("watch_count(): thread %d count p now = %d\n", my_id, p = p + lastPrime ) ;
 		
-		if( p >= TCOUNT )	isFinished = 1 ;
+		if( p >= TCOUNT )	isFinished = 1 ;	// if p >= TCOUNT after adding 
 	}
-	pthread_mutex_unlock( &count_mutex ) ;
+	pthread_mutex_unlock( &count_mutex ) ;	//unlock 
 	pthread_exit( NULL ) ;  
 }
 
 int main( int argc, char *argv[] ){
 	
-	TCOUNT = atoi( argv[1] ) ; 
+	TCOUNT = atoi( argv[1] ) ;	// trans string to num TCOUNT
 	COUNT_LIMIT = atoi( argv[2] ) ; 
 
 	pthread_t threads[NUM_THREAD] ; 
@@ -100,17 +96,19 @@ int main( int argc, char *argv[] ){
 	pthread_mutex_init( &count_mutex, NULL ) ;
 	pthread_cond_init( &count_threshold_cv , NULL ) ; 
 
-	pthread_create( &threads[1], NULL, inc_count, (void*)t1 ) ; 
-	pthread_create( &threads[2], NULL, inc_count, (void*)t2 ) ; 
-	pthread_create( &threads[0], NULL, watch_count, (void*)t3 ) ; 
+	pthread_create( &threads[0], NULL, watch_count, (void*)t1 ) ; 
+	usleep( 200 ) ; 	//delay let thread 1 to listen signal of thread 2 3
+	pthread_create( &threads[1], NULL, inc_count, (void*)t2 ) ; 
+	pthread_create( &threads[2], NULL, inc_count, (void*)t3 ) ; 
+
 	
 	int i ;  
 	for(i = 0; i < NUM_THREAD; ++i ){
-		pthread_join( threads[i], NULL ) ; 
+		pthread_join( threads[i], NULL ) ;	 // join the pthread 
 	}
 	printf("Main() : Waited and joined with %d threads. Final value of count = %d\n", NUM_THREAD, p ) ; 
 	
-	pthread_mutex_destroy( &count_mutex) ;
-	pthread_cond_destroy( &count_threshold_cv ) ; 
-	pthread_exit( NULL ) ;
+	pthread_mutex_destroy( &count_mutex) ;	// destroy mutex
+	pthread_cond_destroy( &count_threshold_cv ) ; // destroy condition 
+	pthread_exit( NULL ) ;	//exit 
 }
