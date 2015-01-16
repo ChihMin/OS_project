c60a64463fbc5502bcd6f49f19434da676e87a2f
ChihMin <s102062111@m102.nthu.edu.tw>
2015-01-16 08:39:32 -0800

    remove some garbage or compile file

--- OS_homework5/.mydev.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chihmin/Desktop/OsHomework/OS_homework5/mydev.o := ld -m elf_x86_64   -r -o /home/chihmin/Desktop/OsHomework/OS_homework5/mydev.o /home/chihmin/Desktop/OsHomework/OS_homework5/main.o 
