894150f5d926284c167e73171871a2dee57aa936
ChihMin <s102062111@m102.nthu.edu.tw>
2015-01-16 08:39:00 -0800

    success allocate a new dev by cdev

---
+++ OS_homework5/.tmp_versions/mydev.mod
@@ -0,0 +1,2 @@
+/home/chihmin/Desktop/OsHomework/OS_homework5/mydev.ko
+/home/chihmin/Desktop/OsHomework/OS_homework5/main.o
--- OS_homework5/main.c
+++ OS_homework5/main.c
@@ -2,11 +2,18 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define EXAMPLE_MAJOR 60
-#define EXAMPLE_NAME "example"
+#define EXAMPLE_NAME "mydev"
+
+static int Major, Minor;
+struct cdev *dev_cdevp = NULL;
+
 
 static int drv_open(struct inode *inode, struct file *filp){
 	printk("OS_HW5:%s():device open\n",__FUNCTION__);
@@ -19,11 +26,8 @@ static int drv_release(struct inode *inode, struct file *flip){
 }
 
 static ssize_t drv_read(struct file *flip, char *buf, size_t size, loff_t *f_pos){
-	int i;
 	printk("OS_HW5:%s():device release\n",__FUNCTION__);
-	for(i = 0; i < 10; ++i)
-		buf[i] = '2';
-	
+			
 	return 0;
 }
 
@@ -33,21 +37,53 @@ static ssize_t drv_write(struct file *flip, const char *buf, size_t size, loff_t
 }
 
 static struct file_operations drv_fops = {
+	owner:		THIS_MODULE,
 	open:		drv_open,
 	release:	drv_release,
 	read:		drv_read,
-	write:		drv_write,
+	write:		drv_write
 };
 
 static int init_modules(void){
-	int result;
+	int ret;
+	dev_t dev;
+	
+
 	printk("OS_HW5:%s():.................Start................\n",__FUNCTION__);
 	
+
+	ret = alloc_chrdev_region(&dev, 0, 1, EXAMPLE_NAME); 	
+	if( ret ){
+		printk("OS_HW5:%s(): Can;t alloc chrdev\n",__FUNCTION__);
+		return ret;
+	}
+	Major = MAJOR(dev);
+	Minor = MINOR(dev);
 	/* Register character device */
-	result = register_chrdev(EXAMPLE_MAJOR, EXAMPLE_NAME, &drv_fops);
-	if( result < 0 ){
-		printk("OS_HW5:%s(): Failed to register character device\n",__FUNCTION__);
-		return result;
+	
+	printk("OS_HW5:%s(): register chrdev(%d,%d)\n", __FUNCTION__, Major, Minor);
+	//result = register_chrdev(Major, EXAMPLE_NAME, &drv_fops);
+	dev_cdevp = kzalloc(sizeof(struct cdev), GFP_KERNEL );
+	if( dev_cdevp == NULL ){
+		printk("OS_HW5:%s(): kzalloc failed\n", __FUNCTION__);
+		goto failed;
+	}
+	
+	cdev_init(dev_cdevp, &drv_fops);
+	dev_cdevp->owner = THIS_MODULE;
+	ret = cdev_add(dev_cdevp, MKDEV(Major, Minor), 1);
+	if( ret < 0){
+		printk("OS_HW5:%s():add chr dev failed\n", __FUNCTION__);
+		goto failed;
+	}
+
+	return 0;
+
+failed:
+
+	if(dev_cdevp) {
+		kfree(dev_cdevp);
+		dev_cdevp = NULL;
 	}
 
 	return 0;
@@ -56,7 +92,14 @@ static int init_modules(void){
 static void exit_modules(void){
 	printk("OS_HW5:%s():unregister chrdev\n",__FUNCTION__);
 	/*  Unregister character device */
-	unregister_chrdev(EXAMPLE_MAJOR, EXAMPLE_NAME);
+	dev_t dev;
+
+	dev = MKDEV(Major, Minor);
+	if(dev_cdevp){
+		cdev_del(dev_cdevp);
+		kfree(dev_cdevp);
+	}
+	unregister_chrdev(Major, EXAMPLE_NAME);
 
 	printk("OS_HW5:%s():..................End.................\n",__FUNCTION__);
 }
